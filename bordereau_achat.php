ea471d3eb3d1aa6d6c71af0f0386c435c20a59dd
jm-metalcash <jm@metalcash.be>
2024-10-30 13:31:41 +0100

    add border

--- bordereau_achat.php
+++ bordereau_achat.php
@@ -106,113 +106,114 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     </div>
                 </div>
 
-                <div class="container bordereau" id="bordereau-<?php echo $packageId; ?>">
-
-                    <header>
-                        <div class="header-barcode">
-                            <div class="header-title">
-                                <h1>Bordereau d'achat</h1>
-                            </div>
-                            <div class="barcode-section">
-                                <p class="date">Créé le <?php echo date('d/m/Y'); ?></p>
-                                <!-- Code-barres général à conserver -->
-                                <img src="<?php echo $relativeBarcodePathGeneral; ?>" alt="Metalcash - Bordereau ID" />
-                                <p class="id_barcode"><?php echo $uniqueId; ?></p>
-                            </div>
-                        </div>
-
-                        <div class="header-info">
-                            <p class="header-info-p">Ce bordereau d’achat est à inclure dans votre colis à destination de l’adresse suivante :</p>
-                            <div class="header-address">
-                                <h2>METALCASH / SERVICE D'ENVOI POSTAL</h2>
-                                <p>Avenue André Ernst 3A, 4800 Verviers, Belgique</p>
-                            </div>
-                        </div>
-                        <p class="note">NOTE: Les colis pesant moins de 10 kg ne sont pas acceptés (voir les conditions en bas de page).</p>
-                    </header>
-
-                    <main>
-                        <section class="beneficiary-info">
-                            <div class="column">
-                                <h3>Identification du bénéficiaire</h3>
-                                <div class="info-grid">
-                                    <p>Prénom: <span><?php echo $formData['firstname']; ?></span></p>
-                                    <p>Nom de famille: <span><?php echo $formData['lastname']; ?></span></p>
-                                    <p>Rue: <span><?php echo $formData['address']; ?></span></p>
-                                    <p>CP, Localité: <span><?php echo $formData['postalCode'] . ', ' . $formData['locality']; ?></span></p>
-                                    <p>Pays: <span><?php echo $formData['country']; ?></span></p>
-                                    <p>Téléphone: <span><?php echo $formData['phone']; ?></span></p>
-                                    <p>E-mail: <span><?php echo $formData['email']; ?></span></p>
+                <div class="pdf-container">
+                    <div class="container bordereau" id="bordereau-<?php echo $packageId; ?>">
+                        <header>
+                            <div class="header-barcode">
+                                <div class="header-title">
+                                    <h1>Bordereau d'achat</h1>
+                                </div>
+                                <div class="barcode-section">
+                                    <p class="date">Créé le <?php echo date('d/m/Y'); ?></p>
+                                    <!-- Code-barres général à conserver -->
+                                    <img src="<?php echo $relativeBarcodePathGeneral; ?>" alt="Metalcash - Bordereau ID" />
+                                    <p class="id_barcode"><?php echo $uniqueId; ?></p>
                                 </div>
                             </div>
-                            <div class="column">
-                                <h3>Coordonnées bancaires</h3>
-                                <div class="info-grid">
-                                    <p>Titulaire du compte: <span><?php echo $formData['firstname'] . ' ' . $formData['lastname']; ?></span></p>
-                                    <p>IBAN: <span><?php echo $formData['iban']; ?></span></p>
-                                    <p>Nom de la banque: <span><?php echo $formData['bankName']; ?></span></p>
-                                    <p>SWIFT: <span><?php echo $formData['swift']; ?></span></p>
-                                    <div class="card-info">
-                                        <p>Numéro de la carte d'identité: <span><?php echo $formData['idCard']; ?></span></p>
-                                        <p>Date d'expiration de la carte d'identité: <span><?php echo $formData['expiryDate']; ?></span></p>
+
+                            <div class="header-info">
+                                <p class="header-info-p">Ce bordereau d’achat est à inclure dans votre colis à destination de l’adresse suivante :</p>
+                                <div class="header-address">
+                                    <h2>METALCASH / SERVICE D'ENVOI POSTAL</h2>
+                                    <p>Avenue André Ernst 3A, 4800 Verviers, Belgique</p>
+                                </div>
+                            </div>
+                            <p class="note">NOTE: Les colis pesant moins de 10 kg ne sont pas acceptés (voir les conditions en bas de page).</p>
+                        </header>
+
+                        <main>
+                            <section class="beneficiary-info">
+                                <div class="column">
+                                    <h3>Identification du bénéficiaire</h3>
+                                    <div class="info-grid">
+                                        <p>Prénom: <span><?php echo $formData['firstname']; ?></span></p>
+                                        <p>Nom de famille: <span><?php echo $formData['lastname']; ?></span></p>
+                                        <p>Rue: <span><?php echo $formData['address']; ?></span></p>
+                                        <p>CP, Localité: <span><?php echo $formData['postalCode'] . ', ' . $formData['locality']; ?></span></p>
+                                        <p>Pays: <span><?php echo $formData['country']; ?></span></p>
+                                        <p>Téléphone: <span><?php echo $formData['phone']; ?></span></p>
+                                        <p>E-mail: <span><?php echo $formData['email']; ?></span></p>
                                     </div>
                                 </div>
+                                <div class="column">
+                                    <h3>Coordonnées bancaires</h3>
+                                    <div class="info-grid">
+                                        <p>Titulaire du compte: <span><?php echo $formData['firstname'] . ' ' . $formData['lastname']; ?></span></p>
+                                        <p>IBAN: <span><?php echo $formData['iban']; ?></span></p>
+                                        <p>Nom de la banque: <span><?php echo $formData['bankName']; ?></span></p>
+                                        <p>SWIFT: <span><?php echo $formData['swift']; ?></span></p>
+                                        <div class="card-info">
+                                            <p>Numéro de la carte d'identité: <span><?php echo $formData['idCard']; ?></span></p>
+                                            <p>Date d'expiration de la carte d'identité: <span><?php echo $formData['expiryDate']; ?></span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+
+                            <section class="table-section">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Type de métaux</th>
+                                            <th>Description <span class="th-desc">(facultatif)</span></th>
+                                            <th>Poids en kg</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        foreach ($materials as $index => $type) {
+                                            $description = $_POST['description'][$packageId][$index] ?? 'N/A';
+                                            $weight = $_POST['weight'][$packageId][$index] ?? 'N/A';
+                                            echo '<tr>';
+                                            echo '<td>' . ($index + 1) . '</td>';
+                                            echo '<td>' . htmlspecialchars($type) . '</td>';
+                                            echo '<td>' . htmlspecialchars($description) . '</td>';
+                                            echo '<td>' . htmlspecialchars($weight) . '</td>';
+                                            echo '</tr>';
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </section>
+                        </main>
+
+                        <footer>
+                            <div class="footer-content">
+                                <p class="accept-conditions">J’ai pris connaissance des conditions générales et de la déclaration de confidentialité et je les accepte.</p>
+                                <p class="certificate"><strong>Certificat de propriété :</strong><br>En tant que personne majeure disposant pleinement de ma capacité juridique, je certifie sur l'honneur que le matériel proposé à la vente par l'entreprise Metalcash sprl m’appartient de manière exclusive et sans restriction, qu'il n'est issu d'aucune activité illégale, qu'il n'est ni mis en gage ni cédé, et qu'il ne contient aucune composante dangereuse.</p>
+                                <p class="conditions"><strong>Lire les conditions générales et la déclaration de confidentialité :</strong><br><a href="https://www.metalcash.be/faq">https://www.metalcash.be/faq</a> — <a href="https://www.metalcash.be/legal">https://www.metalcash.be/legal</a></p>
                             </div>
-                        </section>
+                            <div class="signature-section">
+                                <div class="signature-line"></div>
+                                <p>Date</p>
+                                <div class="signature-line"></div>
+                                <p>Signature</p>
+                            </div>
+                        </footer>
 
-                        <section class="table-section">
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Type de métaux</th>
-                                        <th>Description <span class="th-desc">(facultatif)</span></th>
-                                        <th>Poids en kg</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                    foreach ($materials as $index => $type) {
-                                        $description = $_POST['description'][$packageId][$index] ?? 'N/A';
-                                        $weight = $_POST['weight'][$packageId][$index] ?? 'N/A';
-                                        echo '<tr>';
-                                        echo '<td>' . ($index + 1) . '</td>';
-                                        echo '<td>' . htmlspecialchars($type) . '</td>';
-                                        echo '<td>' . htmlspecialchars($description) . '</td>';
-                                        echo '<td>' . htmlspecialchars($weight) . '</td>';
-                                        echo '</tr>';
-                                    }
-                                    ?>
-                                </tbody>
-                            </table>
-                        </section>
-                    </main>
+                        <div class="content-cut-out">
+                            <i class="fa-solid fa-scissors scissors-icon"></i>
+                            <p>Merci de détacher cette partie et de l'apposer sur l'emballage du colis</p>
+                            <div class="informations-cut">
+                                <!-- Code-barres spécifique au colis -->
+                                <img src="<?php echo $relativeBarcodePath; ?>" alt="Metalcash-package-id" />
+                                <span class="cut-uniqueId"><?php echo $currentUniqueId ?></span>
+                                <span class="cut-packageId">Colis <?php echo $packageId ?> / <?php echo count($_POST['materialType']); ?></span>
+                            </div>
+                        </div>
 
-                    <footer>
-                        <div class="footer-content">
-                            <p class="accept-conditions">J’ai pris connaissance des conditions générales et de la déclaration de confidentialité et je les accepte.</p>
-                            <p class="certificate"><strong>Certificat de propriété :</strong><br>En tant que personne majeure disposant pleinement de ma capacité juridique, je certifie sur l'honneur que le matériel proposé à la vente par l'entreprise Metalcash sprl m’appartient de manière exclusive et sans restriction, qu'il n'est issu d'aucune activité illégale, qu'il n'est ni mis en gage ni cédé, et qu'il ne contient aucune composante dangereuse.</p>
-                            <p class="conditions"><strong>Lire les conditions générales et la déclaration de confidentialité :</strong><br><a href="https://www.metalcash.be/faq">https://www.metalcash.be/faq</a> — <a href="https://www.metalcash.be/legal">https://www.metalcash.be/legal</a></p>
-                        </div>
-                        <div class="signature-section">
-                            <div class="signature-line"></div>
-                            <p>Date</p>
-                            <div class="signature-line"></div>
-                            <p>Signature</p>
-                        </div>
-                    </footer>
-
-                    <div class="content-cut-out">
-                        <i class="fa-solid fa-scissors scissors-icon"></i>
-                        <p>Merci de détacher cette partie et de l'apposer sur l'emballage du colis</p>
-                        <div class="informations-cut">
-                            <!-- Code-barres spécifique au colis -->
-                            <img src="<?php echo $relativeBarcodePath; ?>" alt="Metalcash-package-id" />
-                            <span class="cut-uniqueId"><?php echo $currentUniqueId ?></span>
-                            <span class="cut-packageId">Colis <?php echo $packageId ?> / <?php echo count($_POST['materialType']); ?></span>
-                        </div>
                     </div>
-
                 </div>
         <?php
             }
